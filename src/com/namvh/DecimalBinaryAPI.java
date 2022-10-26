7aa6f718ef6a00b9435cff453966ebc8b30c034c
xbsvhn <aheadfuture2000@gmail.com>
2022-10-26 19:54:52 +0700

    decimal to binary algorithm

---
+++ src/com/namvh/DecimalBinaryAPI.java
@@ -0,0 +1,16 @@
+package com.namvh;
+
+/**
+ * @author namvh
+ */
+public class DecimalBinaryAPI {
+
+        public static void main(String a[]){
+            System.out.println("Binary representation of 8: ");
+            System.out.println(Integer.toBinaryString(8));
+            System.out.println("\nBinary representation of 45: ");
+            System.out.println(Integer.toBinaryString(45));
+            System.out.println("\nBinary representation of 999: ");
+            System.out.println(Integer.toBinaryString(999));
+        }
+    }
---
+++ src/com/namvh/DecimalBinaryStack.java
@@ -0,0 +1,34 @@
+package com.namvh;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * @author namvh
+ */
+public class DecimalBinaryStack {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // Create Stack object
+        Stack<Integer> stack = new Stack<Integer>();
+
+        // User input
+        System.out.println("Enter decimal number: ");
+        int num = in.nextInt();
+
+        while (num != 0) {
+            int d = num % 2;
+            stack.push(d);
+            num /= 2;
+        }
+
+        System.out.print("\nBinary representation is:");
+        while (!(stack.isEmpty())) {
+            System.out.print(stack.pop());
+        }
+        System.out.println();
+    }
+
+}
